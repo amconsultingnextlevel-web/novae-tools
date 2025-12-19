7c0c2cecbc2e277aee060446c414d1c7888a70a6
Mark Wagner <youremail@example.com>
2025-12-19 11:31:08 -0700

    Add Phase 2 configurator route

---
+++ app/configurator/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import React from "react";
+import TrailerConfigurator from "../components/TrailerConfigurator";
+
+export default function ConfiguratorPage() {
+  return <TrailerConfigurator />;
+}
